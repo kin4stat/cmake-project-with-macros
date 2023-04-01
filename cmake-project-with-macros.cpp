4da5af48c6b3896e16a2df9806ed2ee0b384ca04
kin4stat <dkustov4@gmail.com>
2023-04-01 15:42:54 +0300

    MRE

--- cmake-project-with-macros.cpp
+++ cmake-project-with-macros.cpp
@@ -1,18 +1,4 @@
-﻿// cmake-project-with-macros.cpp : Defines the entry point for the application.
-//
-
-#include "cmake-project-with-macros.h"
-
-using namespace std;
-
-int main()
-{
-	cout << "Hello CMake." << endl;
-	my::my_func(); 
-	return 0;
-}
-
-MY_NAMESPACE_BEGIN
+﻿MY_NAMESPACE_BEGIN
 void my_func()
 {
 }
