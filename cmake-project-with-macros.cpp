d7d810ca14a579616f8ed02d1897404d4e1faa96
Anatoly Shirokov <anatoly.shirokov@gmail.com>
2023-04-01 14:53:37 +0300

    Add sources

---
+++ cmake-project-with-macros.cpp
@@ -0,0 +1,19 @@
+﻿// cmake-project-with-macros.cpp : Defines the entry point for the application.
+//
+
+#include "cmake-project-with-macros.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	my::my_func(); 
+	return 0;
+}
+
+MY_NAMESPACE_BEGIN
+void my_func()
+{
+}
+MY_NAMESPACE_END
